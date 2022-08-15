eeda696ea86f1c569b3485b26fbd71933236c395
johguentner <johannes.guentner@it-guentner.de>
2022-08-15 17:55:57 +0200

    add: full kanji-listing and links to helpers - link for every kanji to kanjidamage - link for every kanji to jisho (sentence examples) - overview of word and each kanji, which is within the word (fetched from api) - tailwindcss designed overlay (styling)

--- main.ts
+++ main.ts
@@ -17,6 +17,8 @@ import { hoverTooltip } from "@codemirror/tooltip";
 
 import axios from 'axios';
 
+import { findIndex, join } from 'lodash';
+
 
 document.addEventListener('mousemove', (event) => {
 	window.clientX = event.clientX;
@@ -52,9 +54,6 @@ export default class MyPlugin extends Plugin {
 		statusBarItemEl.setText('Status Bar Text');
 
 
-
-
-
 		const wordHover = hoverTooltip(async (view, pos, side) => {
 			let { from, to, text } = view.state.doc.lineAt(pos);
 
@@ -69,47 +68,119 @@ export default class MyPlugin extends Plugin {
 				end++;
 			}
 			if ((start === pos && side < 0) || (end === pos && side > 0)) {
-
-
 				return null;
 			}
 
 			let dom = document.createElement("div");
-			dom.style.padding = "10px";
-			dom.style.border = "1px solid lightgray";
-			dom.style.borderRadius = "4px";
 			dom.style.position = "absolute";
-			dom.style.backgroundColor = "white";
 			dom.style.zIndex = "1000";
-			dom.style.left = window.clientX + "px";
-			dom.style.top = window.clientY + "px";
-			dom.className = "cm-tooltip-cursor"
-			let kanji = view.state.doc.slice(start, end);
+			dom.style.left = (window.clientX) + "px";
+			dom.style.top = (window.clientY + 20) + "px";
+			let kanjies = (view.state.doc.slice(start, end)).text[0];
 
-			let firstKanji = kanji.text[0][0];
+			let firstKanji = kanjies[0];
 
 
-			let apiUrl = "https://kanjiapi.dev/v1/kanji/" + firstKanji;
-			let result = await axios.get(apiUrl);
+			let wordsUrl = "https://kanjiapi.dev/v1/words/" + firstKanji;
+			let wordsResult = await axios.get(wordsUrl);
 
-			if (result.status === 200) {
-				let results = result.data;
-				let kunReadings = results.kun_readings;
-				dom.textContent = kunReadings[0];
+			let word = null;
+			let kanjiResults = [];
+
+			for (let kanji of kanjies) {
+				let kanjiUrl = "https://kanjiapi.dev/v1/kanji/" + kanji;
+				let kanjiResult = await axios.get(kanjiUrl);
+				if (kanjiResult.status === 200) {
+					let results = kanjiResult.data;
+					kanjiResults.push(results);
+				}
 			}
-			else {
-				return null;
+
+
+			if (wordsResult.status === 200) {
+				let wordIndex = findIndex(wordsResult.data, (o) => {
+					return o.variants[0].written == kanjies;
+				});
+
+				if (wordIndex !== -1) {
+					word = wordsResult.data[wordIndex];
+				}
+			}
+
+			let wordPart = "";
+			let kanjiPart = "";
+
+			if (word) {
+				let wordInfo = word.variants[0];
+				let wordMeaning = join(word.meanings[0].glosses, ", ");
+				wordPart =
+					`<div class="relative pb-3 text-base leading-7 text-gray-600">
+						<a class="absolute top-4 right-0 text-xs" href="https://jisho.org/search/${wordInfo.written}%20%23sentences">
+							<svg class="w-4 h-4 text-gray-300 duration-300 hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+						</a>
+						<div class="flex items-center gap-7 text-2xl">${wordInfo.written} <small>${wordInfo.pronounced}</small></div>
+						<p class="text-sm">${wordMeaning}</p>  
+					</div>`;
+			}
+
+			for (let kanji of kanjiResults) {
+				kanjiPart +=
+					`<div class="relative py-3 text-base leading-7 text-gray-600">
+						<a class="absolute top-4 right-0 text-xs" href="https://www.kanjidamage.com/kanji/search?q=${kanji.kanji}">
+							<svg class="w-4 h-4 text-gray-300 duration-300 hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"></path></svg>
+						</a>
+						<div class="flex items-center gap-7 text-2xl">${kanji.kanji}</div>
+						<p>${join(kanji.kun_readings, ", ")}</p>  
+						<p>${join(kanji.on_readings, ", ")}</p>  
+						<p class="text-sm">${join(kanji.meanings, ", ")}</p>  
+					</div>`;
 			}
 
 
-			if (window.kanji_tooltip) {
-				document.body.removeChild(window.kanji_tooltip);
+
+			dom.innerHTML = `
+				<div class="relative bg-white py-6 shadow-lg ring-1 ring-gray-900/5 sm:max-w-lg sm:rounded-lg px-7">
+					<button class="absolute top-3 right-3" onclick="window.obsidian_kanji_jisho.close()">
+						<svg class="w-5 h-5 text-gray-300 hover:text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+					</button>
+					<div class="mx-auto max-w-md">
+					<div class="divide-y divide-gray-300/50">
+						${wordPart}
+						${kanjiPart}
+					</div>
+					</div>	
+				</div>`;
+
+			function hideOnClickOutside(element) {
+				const outsideClickListener = event => {
+					if (!element.contains(event.target) && isVisible(element)) { // or use: event.target.closest(selector) === null
+						element.style.display = 'none';
+						removeClickListener();
+					}
+				}
+
+				const removeClickListener = () => {
+					document.removeEventListener('click', outsideClickListener);
+				}
+
+				document.addEventListener('click', outsideClickListener);
+			}
+
+			const isVisible = elem => !!elem && !!(elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length); // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
+
+			if (window.obsidian_kanji_jisho?.kanji_tooltip) {
+				document.body.removeChild(window.obsidian_kanji_jisho.kanji_tooltip);
 			}
 
 			document.body.appendChild(dom);
-			window.kanji_tooltip = dom;
+			window.obsidian_kanji_jisho = {};
+			window.obsidian_kanji_jisho.kanji_tooltip = dom;
+			window.obsidian_kanji_jisho.close = () => {
+				document.body.removeChild(window.obsidian_kanji_jisho.kanji_tooltip);
+				window.obsidian_kanji_jisho.kanji_tooltip = null;
+			};
 
-
+			hideOnClickOutside(dom);
 
 			return null;
 		});
@@ -117,29 +188,9 @@ export default class MyPlugin extends Plugin {
 
 		this.registerEditorExtension([wordHover]);
 
-
-
-		this.registerMarkdownPostProcessor((element) => {
+		// this.registerMarkdownPostProcessor((element) => {
 			//TODO: FOR LATER !!
-			console.log("TEST");
-			// We only want to add tooltips to:
-			//  1. external links
-			//  2. links which don't already show the href
-			// const targetLinks = Array.from(element.getElementsByTagName("a")).filter(
-			//   (link) =>
-			// 	link.classList.contains("external-link") &&
-			// 	link.href !== link.innerHTML
-			// );
-
-			new Notice('This is a notice!');
-
-			// for (const link of targetLinks) {
-			//   tippy(link, {
-			// 	content: link.href,
-			//   });
-			// }
-		});
-
+		// });
 
 
 		// This adds a simple command that can be triggered anywhere
